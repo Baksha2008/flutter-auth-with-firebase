cc8bac26ab2a0ea6f586fcce94cbb26a244f76e3
Baksha2008 <baksha2008@gmail.com>
2020-05-06 00:16:13 +0300

    init project and firebase

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:auth_flutter/screens/auth/auth.dart';
+import 'package:auth_flutter/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return auth or home
+    return Auth();
+  }
+}
